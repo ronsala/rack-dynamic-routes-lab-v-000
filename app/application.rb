7336f2d9eba5b40659ea0a2449e5b53797a3b9e8
Ron Sala <ron.sala@gmail.com>
2018-06-26 02:16:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,21 +4,14 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    # if req.path.match(/items/<ITEM NAME>)
-    # if req.path == '/items/<ITEM NAME>'
-    # if req.path == '/items/'
-    # if req.path.include?("items")
-    # if req.path=="/items"
     if req.path.match(/items/)
       # item = req.params["item"]
       item_name = req.path.split("/items/").last
       # if @@items.include?(item)
       if @@items.find{|i| i.name == item_name}
         req_item = @@items.find{|i| i.name == item_name}
-
-
         resp.write "#{req_item.price}"
-        resp.status = 200
+        # resp.status = 200
       else
         resp.write "Item not found"
         resp.status = 400
